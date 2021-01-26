ed1af443d5dde2c115bfe523dc63bc7f2e211881
alexandrugoncearu@privent.onmicrosoft.com <alexandrugoncearu@privent.onmicrosoft.com>
2021-01-26 15:42:15 +0200

    initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
+import 'package:armenu_app/screens/VirtualTable/virtualTable.dart';
 import 'package:flutter/material.dart';
-import 'package:armenu_app/screens/login/login.dart';
-import 'package:armenu_app/screens/menu/menu.dart';
-import 'package:armenu_app/screens/qrcode/qrcode.dart';
-import 'package:armenu_app/screens/settings/settings.dart';
-import 'package:armenu_app/screens/virtualtable/virtualTable.dart';
-import 'package:armenu_app/screens/waiter/waiter.dart';
-import 'package:armenu_app/utils/styles.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
   theme: ThemeData(fontFamily: 'Roboto', hintColor: Color(0xFFd0cece)),
-  home: HomePage(),
+  home: VirtualTable(),
 ));
 
