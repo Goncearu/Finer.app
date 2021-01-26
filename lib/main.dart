2ea0565e4fd5f6b5f78620966e484fee510e0717
adelinpreda <adelinpreda@privent.onmicrosoft.com>
2021-01-26 13:36:49 +0200

    purpleAccent

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.black,
+          primaryColor: Colors.purpleAccent,
           scaffoldBackgroundColor: Colors.black,
           cardColor: Colors.white12,
           textTheme: GoogleFonts.latoTextTheme(
