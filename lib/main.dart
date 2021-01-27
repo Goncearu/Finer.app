e83d97dcb06206a13c156f3b151e320741e3d9e6
alexandrugoncearu@privent.onmicrosoft.com <alexandrugoncearu@privent.onmicrosoft.com>
2021-01-27 23:34:58 +0200

    class FinerApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 
 import 'package:flutter/material.dart';
-
 import 'screens/VirtualTable/virtualTable.dart';
 
-void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  theme: ThemeData(fontFamily: 'Roboto', hintColor: Color(0xFFd0cece)),
-  home: VirtualTable(),
-));
+void main() => runApp(FinerApp());
+
+class FinerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Galano',
+        hintColor: Color(0xFFd0cece),
+        ),
+      home: VirtualTable(),
+    );
+  }
+}
+
+
 
